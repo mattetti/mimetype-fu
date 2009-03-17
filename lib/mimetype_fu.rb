18ba3cbe9d31afaceb9199cb9f3bcc484932d688
Philip Mcmahon <philip@packetnode.com>
2009-03-17 14:27:19 -0600

    Getting a nil object error for files with no extension, would prefer an 'unknown/unknown' mimetype

--- lib/mimetype_fu.rb
+++ lib/mimetype_fu.rb
@@ -8,7 +8,7 @@ class File
          mime = EXTENSIONS[File.extname(file.path).gsub('.','').downcase.to_sym]
        end
      elsif file.class == String
-       mime = EXTENSIONS[(file[file.rindex('.')+1, file.size]).downcase.to_sym]
+       mime = EXTENSIONS[(file[file.rindex('.')+1, file.size]).downcase.to_sym] if not file.rindex('.').nil?
      elsif file.class == StringIO
        temp = File.open(Dir.tmpdir + '/upload_file.' + Process.pid.to_s, "wb")
        temp << file.string
