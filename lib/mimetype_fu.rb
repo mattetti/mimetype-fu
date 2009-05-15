efe3ff308af9f6361d938ea8a62e502b6022ded4
Josh French <josh@digitalpulp.com>
2009-05-15 12:21:55 -0400

    Tempfile support (Rack)

--- lib/mimetype_fu.rb
+++ lib/mimetype_fu.rb
@@ -1,35 +1,30 @@
 class File
 
   def self.mime_type?(file)
-     if file.class == File
-       unless RUBY_PLATFORM.include? 'mswin32'
-         mime = `file --mime -br "#{file.path}"`.strip
-       else
-         mime = EXTENSIONS[File.extname(file.path).gsub('.','').downcase.to_sym]
-       end
-     elsif file.class == String
-       mime = EXTENSIONS[(file[file.rindex('.')+1, file.size]).downcase.to_sym] if not file.rindex('.').nil?
-     elsif file.class == StringIO
-       temp = File.open(Dir.tmpdir + '/upload_file.' + Process.pid.to_s, "wb")
-       temp << file.string
-       temp.close
-       mime = `file --mime -br "#{temp.path}"`
-       mime = mime.gsub(/^.*: */,"")
-       mime = mime.gsub(/;.*$/,"")
-       mime = mime.gsub(/,.*$/,"")
-       File.delete(temp.path)
-     end
-
-     if mime
-       return mime
-     else
-       'unknown/unknown'
-     end
+    case file
+    when File, Tempfile
+      unless RUBY_PLATFORM.include? 'mswin32'
+        mime = `file --mime -br "#{file.path}"`.strip
+      else
+        mime = EXTENSIONS[File.extname(file.path).gsub('.','').downcase.to_sym]
+      end
+    when String
+      mime = EXTENSIONS[(file[file.rindex('.')+1, file.size]).downcase.to_sym] unless file.rindex('.').nil?
+    when StringIO
+      temp = File.open(Dir.tmpdir + '/upload_file.' + Process.pid.to_s, "wb")
+      temp << file.string
+      temp.close
+      mime = `file --mime -br "#{temp.path}"`
+      mime = mime.gsub(/^.*: */,"")
+      mime = mime.gsub(/;.*$/,"")
+      mime = mime.gsub(/,.*$/,"")
+      File.delete(temp.path)
+    end
+    return mime || 'unknown/unknown'
    end
 
-  
   def self.extensions
     EXTENSIONS
   end
-  
+
 end
