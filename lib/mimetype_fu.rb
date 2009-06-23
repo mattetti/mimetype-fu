06005fc0a66028ac87b1d6892c97049a8092e751
Tom Harris <thomas.e.harris@gmail.com>
2009-06-23 16:20:34 -0400

    Added support for files with spaces in name.

--- lib/mimetype_fu.rb
+++ lib/mimetype_fu.rb
@@ -3,7 +3,7 @@ class File
   def self.mime_type?(file)
      if file.class == File
        unless RUBY_PLATFORM.include? 'mswin32'
-         mime = `file --mime -br #{file.path}`.strip
+         mime = `file --mime -br "#{file.path}"`.strip
        else
          mime = EXTENSIONS[File.extname(file.path).gsub('.','').downcase.to_sym]
        end
@@ -13,7 +13,7 @@ class File
        temp = File.open(Dir.tmpdir + '/upload_file.' + Process.pid.to_s, "wb")
        temp << file.string
        temp.close
-       mime = `file --mime -br #{temp.path}`
+       mime = `file --mime -br "#{temp.path}"`
        mime = mime.gsub(/^.*: */,"")
        mime = mime.gsub(/;.*$/,"")
        mime = mime.gsub(/,.*$/,"")
--- spec/mime_type_spec.rb
+++ spec/mime_type_spec.rb
@@ -2,14 +2,14 @@ require File.dirname(__FILE__) + '/spec_helper'
 require File.dirname(__FILE__) + '/../lib/mimetype_fu'
 
 # Ideally, the two solutions (`file` command versus simply looking at the extension)
-# would return identical results, but thats not the case. `file` differentiates 
+# would return identical results, but thats not the case. `file` differentiates
 # between file sizes, and has a tendency to classify files into the 'application/octet-stream'
 # mimetype, whereas, looking at the ext results in 'unknown/unknown' more often.
 
 describe "An open file" do
-  
+
   describe "with a zero filelength" do
-  
+
     before {create_file('file.empty')}
 
     it 'should have a length of zero' do
@@ -17,9 +17,9 @@ describe "An open file" do
     end
 
     it_should_have_a_mime_type_of("application/x-empty")
-    
+
   end
-  
+
   describe 'with a known extension' do
 
     before {write_file('file.png', "\211PNG\r\n\032\n", 'wb')}
@@ -32,7 +32,21 @@ describe "An open file" do
     it_should_have_a_mime_type_of("image/png")
 
   end
-    
+
+  describe 'with spaces in name' do
+
+    before {write_file('file with spaces.png', "\211PNG\r\n\032\n", 'wb')}
+
+    it 'should have an extension' do
+      File.extname(@file.path).should == '.png'
+    end
+
+    it 'should have a mime type' do
+     File.mime_type?(@file).should == "image/png"
+    end
+
+  end
+
   describe 'with an unknown extension' do
 
     before {write_file('file.unknown', "\211Random\r\n\032\n", 'wb')}
@@ -45,7 +59,7 @@ describe "An open file" do
     it_should_have_a_mime_type_of("application/octet-stream")
 
   end
-  
+
   describe 'with no extension' do
 
     before{write_file('file', "Random\r\n\032\n", 'wb')}
@@ -57,23 +71,23 @@ describe "An open file" do
     it_should_have_a_mime_type_of("application/octet-stream")
 
   end
-  
+
 end
 
 describe "A filepath (closed file)" do
-  
+
   after(:each) do
     delete_file
   end
-  
+
   describe "with a zero filelength" do
-  
+
     before {create_file('file.empty'); close}
 
     it_should_have_a_mime_type_of("unknown/unknown")
-    
+
   end
-  
+
   describe 'with a known extension' do
 
     before {write_file('file.png', "\211PNG\r\n\032\n", 'wb'); close}
@@ -86,7 +100,7 @@ describe "A filepath (closed file)" do
     it_should_have_a_mime_type_of("image/png")
 
   end
-    
+
   describe 'with an unknown extension' do
 
     before {write_file('file.unknown', "\211Random\r\n\032\n", 'wb'); close}
@@ -99,7 +113,7 @@ describe "A filepath (closed file)" do
     it_should_have_a_mime_type_of("unknown/unknown")
 
   end
-  
+
   describe 'with no extension' do
 
     before{write_file('file', "Random\r\n\032\n", 'wb'); close}
